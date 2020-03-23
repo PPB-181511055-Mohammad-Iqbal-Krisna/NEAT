ff4775e90ca06d361de86b979f07bf96b534fe77
kresnaiqbal <mohammad.iqbal.krisna.tif18@polban.ac.id>
2020-03-23 19:18:59 +0700

    second commit

--- app/src/main/java/com/example/neat/MainActivity.java
+++ app/src/main/java/com/example/neat/MainActivity.java
@@ -53,81 +53,6 @@ public class MainActivity extends Activity {
 //        myDataset.add();
         recyclerView.setAdapter(mAdapter);
         textBox = (EditText) findViewById(R.id.editText);
-
-        public void onClickLoad(View view) {
-            Intent i = new Intent("com.example.AppPreferenceActivity");
-            startActivity(i);
-        }
-
-        public void onClickDisplay(View view) {
-            SharedPreferences appPrefs =
-                    getSharedPreferences("com.jfdimarzio.usingpreferences_preferences", MODE_PRIVATE);
-            DisplayText(appPrefs.getString("editTextPref", ""));
-        }
-
-        public void onClickModify(View view){
-            SharedPreferences appPrefs =
-                    getSharedPreferences("com.jfdimarzio.usingpreferences_preferences", MODE_PRIVATE);
-            SharedPreferences.Editor prefsEditor = appPrefs.edit();
-            prefsEditor.putString("editTextPref",
-                    ((EditText) findViewById(R.id.editText)).getText().toString());
-            prefsEditor.commit();
-        }
-
-        public void DisplayText (String Object str;) {
-            Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG).show();
-        }
-
-        public void onClickSave(View view) {
-            String str = textBox.getText().toString();
-            try {
-                FileOutputStream fOut = openFileOutput("textfile.txt",  MODE_PRIVATE);
-                OutputStreamWriter osw = new OutputStreamWriter(fOut);
-                //---write the string to the file---
-                try {
-                    osw.write(str);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                osw.flush();
-                osw.close();
-                //---display file saved message---
-                Toast.makeText(getBaseContext(),
-                        "File saved successfully!", Toast.LENGTH_SHORT).show();
-                //---clears the EditText---
-                textBox.setText("");
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-            }
-        }
-
-        public void onClickLoad(View view) {
-            try {
-                FileInputStream fIn = openFileInput("textfile.txt");
-                InputStreamReader isr = new InputStreamReader(fIn);
-                char[] inputBuffer = new char[READ_BLOCK_SIZE];
-                String s = "";
-                int charRead;
-                while ((charRead = isr.read(inputBuffer)) > 0) {
-                    //---convert the chars to a String---
-                    String readString =
-                            String.copyValueOf(inputBuffer, 0,
-                                    charRead);
-                    s += readString;
-                    inputBuffer = new char[READ_BLOCK_SIZE];
-                }
-                //---set the EditText to the text that has been
-                // read---
-                textBox.setText(s);
-                Toast.makeText(getBaseContext(),
-                        "File loaded successfully!",
-                        Toast.LENGTH_SHORT).show();
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-            }
-        }
-
-
     }
 
 
@@ -146,6 +71,30 @@ public class MainActivity extends Activity {
                 Task newTask = (Task) data.getSerializableExtra("newTask");
                 myDataset.add(newTask);
             }
+        }
+    }
+
+
+    public void onClickSave(View view) {
+        String str = textBox.getText().toString();
+        try {
+            FileOutputStream fOut = openFileOutput("textfile.txt",  MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+            //---write the string to the file---
+            try {
+                osw.write(str);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            osw.flush();
+            osw.close();
+            //---display file saved message---
+            Toast.makeText(getBaseContext(),
+                    "File saved successfully!", Toast.LENGTH_SHORT).show();
+            //---clears the EditText---
+            textBox.setText("");
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         }
     }
 }
--- app/src/main/java/com/example/neat/MyTaskRecyclerViewAdapter.java
+++ app/src/main/java/com/example/neat/MyTaskRecyclerViewAdapter.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.neat.TaskFragment.OnListFragmentInteractionListener;
 import com.example.neat.dummy.DummyContent.DummyItem;
 
 import java.util.List;
